86dd9523612b97c07cac5019ef966f95c515c164
Blaemerson <blakesavage99@gmail.com>
2023-12-12 08:59:24 -0600

    Add steam rules and powermenu

--- config.h
+++ config.h
@@ -59,6 +59,7 @@ static const Rule rules[] = {
 	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
 	{ "kitty",   NULL,     NULL,           0,         0,          1,           0,        -1 },
 	{ "Sxiv",    NULL,     NULL,           0,         1,          0,           0,        -1 },
+    { "steam",   NULL,     NULL,           1 << 8,    0,          0,           0,        -1 },
 	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
